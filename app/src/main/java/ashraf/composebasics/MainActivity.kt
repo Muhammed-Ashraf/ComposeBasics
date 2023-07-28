7e6d0b3ae0e2cd66c7a9269b1ac40e4c5c8fb3ca
ashraf probook <ashraf@itware.ae>
2023-07-28 12:42:31 +0400

    Lazy Column

--- app/src/main/java/ashraf/composebasics/MainActivity.kt
+++ app/src/main/java/ashraf/composebasics/MainActivity.kt
@@ -3,12 +3,16 @@ package ashraf.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ashraf.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +34,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column() {
-        for (i in 1..10)
-            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+    LazyColumn(Modifier.fillMaxSize()) {
+        items(10) { i ->
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
+            )
+        }
+
     }
 
 
