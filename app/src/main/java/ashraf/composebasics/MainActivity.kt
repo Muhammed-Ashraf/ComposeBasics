f0b315edf96503525113413842f19901dd94fd9b
ashraf probook <ashraf@itware.ae>
2023-07-28 14:51:57 +0400

    Update compose

--- app/src/main/java/ashraf/composebasics/MainActivity.kt
+++ app/src/main/java/ashraf/composebasics/MainActivity.kt
@@ -3,55 +3,39 @@ package ashraf.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ashraf.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            ComposeBasicsTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-            Greeting("Android")
-//                }
-//            }
+            ComposeBasicsTheme {
+                var count by remember { mutableStateOf(0) }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = count.toString(), fontSize = 30.sp)
+                    Button(onClick = { count++ }) {
+                        Text(text = "Click")
+                    }
+                }
+            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    LazyColumn(Modifier.fillMaxSize()) {
-        items(10) { i ->
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = null,
-                modifier = Modifier.size(100.dp)
-            )
-        }
 
-    }
-
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeBasicsTheme {
-        Greeting("Android")
-    }
-}
